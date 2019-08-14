899bcaacdfda81b2d7eac3dbcb8f72ac6912277c
Nik <nik.fuchkin@gmail.com>
2019-08-15 02:47:06 +0300

    modifity

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ function add() {
  //document.getElementById("list").innerHTML = "<li>" + document.getElementById("task").value + "<\li>";
  var ts = document.createElement("li");
  var str = document.createTextNode(document.getElementById("task").value);
- ts.appendChild(str);
- document.getElementById("list").appendChild(ts);
+ if (str != "") {
+    ts.appendChild(str);
+    document.getElementById("list").appendChild(ts);
+ }
+ document.getElementById("task").value = "";
+ document.getElementById("name").innerHTML = "You have to make : ";
 }
-var a = "<li>" + document.getElementById("task").value + "<\li>";    
+//var a = "<li>" + document.getElementById("task").value + "<\li>";    
 
+//= "You have to make : "
