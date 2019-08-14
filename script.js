de892819d9cc3c4f3566069fe5dbe4d18f343cf0
Nik <nik.fuchkin@gmail.com>
2019-08-15 02:29:14 +0300

    Creat list task

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function add() {
-    alert("Hi");
-}
 
+var i = 0;
+function add() {
+ //document.getElementById("list").innerHTML = "<li>" + document.getElementById("task").value + "<\li>";
+ var ts = document.createElement("li");
+ var str = document.createTextNode(document.getElementById("task").value);
+ ts.appendChild(str);
+ document.getElementById("list").appendChild(ts);
+}
+var a = "<li>" + document.getElementById("task").value + "<\li>";    
 
