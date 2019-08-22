3548daa33999f89c0978d7cfb9b9b64797e3fc08
Nik <nik.fuchkin@gmail.com>
2019-08-22 13:11:52 +0300

    toDo_1_task(ok)

--- home/script.js
+++ home/script.js
@@ -1,151 +1,30 @@
-﻿/*
-var editObj = {};
-var col_task = 0;
+﻿function addElement(textTask) {
+        if ( textTask.length != 0 ){
+            $("ul").append("<li id=\"item\">" + "<input type=\"checkbox\" >" + textTask  + "<button id=\"del\"  > del </button></li>");
+            $("input#task").val("");
+        }
+}
+
+function delet(a) {
+    alert("del");
+}
 
 
 $(function () {
-    $(".but").click(function () { 
-        if(editObj.edit == true) {
-            editObj.obj.html("<li id=\"item\"> <input type=\"checkbox\"> " + $(".task").val() + "<button class=\"del\"> del </button> </li>" );
-        }
-        else if($(".task").val().length != 0){
-            
-            // ------------------------------jQuery
-            $("ul").append("<li id=\"item\"> <input type=\"checkbox\"> " + $(".task").val() + "<button class=\"del\"> del </button> </li>");
-            $(".task").val("");
+    var count = 0;
+    var objEdit = {};
+    var redy = false;
 
-            var el = document.createElement("li");
-
-            //-------------------------JS
-            el.setAttribute("id", "item");
-            el.appendChild(document.createElement("input").setAttribute("type", "checkbox"));
-            el.appendChild(document.createTextNode($(".task").val()));
-            var el_del = document.createElement("button");
-            el_del.className = "del";
-
-            el_del.appendChild(document.createTextNode("del"));
-            el.appendChild(el_del);
-
-            //--------------------
-            
-
-            col_task++;
-            $("ul").find("*").on(dblclick, dbevent());
-        
-            //dbevent();
-        }
-        
+    $("button#but").click(function (e) { 
+        addElement($("input#task").val());
     });
-});
 
-
-$(function () { 
-    $('html').keydown(function(e) {
-        if (e.keyCode == 13) { 
-            if($(".task").val().length != 0){
-                $("ul").append("<li id=\"item\"> <input type=\"checkbox\"> " + $(".task").val() + "<button class=\"del\"> del </button> </li>");
-                $(".task").val("");
-                col_task++;
-                //dbevent();
-            }
+    $(document).keydown(function (e) { 
+        if(e.code == "Enter") {
+         addElement($("input#task").val());
         }
-      });
-});
-
-
-$(function (this) { 
+    });
     
- });
 
-*/
-
-$(function () { 
-    var obdEdit = {};
-    var counter = 0;
-    var taskList = $("ul");
-    var inputTask = $(".task");
-
-    $(".but").on(onclick, addTheTask());
-
-    var addTheTask = function () { 
-        let element = document.createElement("li");
-        element.appendChild(document.createElement("input").setAttribute("type", "checkbox"));
-        element.appendChild(document.createTextNode(inputTask.val()));
-
-        let delButton = document.createElement("button");
-        delButton.appendChild(document.createTextNode("del"));
-        delButton.className = "del";
-        delButton.onclick = "deletTask(this)";
-        element.appendChild(delButton);
-        element.ondblclick = "edit(this)";
-        taskList.append(element);
-    }
-
-    var deletTask = function (this) { 
-        $(this).parent.removeChild(this);
-    }
-
-
-    var edit = function () { 
-        obdEdit.edit = true;
-        obdEdit.obj = this;
-    }
-
-
-
- })
-
-
-
-
-
-
-
-
-/*
-$(function chekEl() {
-    $("#all_check").change(function () { 
-        if($("#all_check").text() == "on"){
-            $("p").hide();
-        }
-        else
-        {
-            $("p").show();
-        } 
-})
-
-/*var col_task = 0;
-const input = document.querySelector('input');
-input.onkeyup = logKey;
-
-function add() {
-    var str = document.createTextNode(document.getElementById("task").value);
-    if (str.length != 0) {
-        var ts = document.createElement("li");
-        var del = document.createElement("button");
-        del.setAttribute("onclick", "del(this)");
-        del.appendChild(document.createTextNode("del"));
-        ts.appendChild(str);
-        ts.appendChild(del);
-        document.getElementById("tasks").appendChild(ts);
-        col_task += 1;
-        document.getElementById("task").value = "";
-    }
-}
-
-function del(element) {
-    var el = element;
-    el.parentNode.parentNode.removeChild(el.parentNode);
-    col_task+=1;
-}
-
-//Listing keyup
-
-function logKey(e) {
-    if (e.code == "Enter") {
-        add();
-    }
-}
-*/
-
-
+   
+});
