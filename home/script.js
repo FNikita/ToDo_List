2f7514c9acce2b821bb02874586291203deef815
Nik <nik.fuchkin@gmail.com>
2019-08-22 17:39:04 +0300

    done nedone

--- home/script.js
+++ home/script.js
@@ -1,13 +1,15 @@
-﻿function addElement(textTask) {
+﻿
+function addElement(textTask) {
         if ( textTask.length != 0 ){
-            $("ul").append(`<li class="item"><input type="checkbox" class="check"> ${textTask} <button class="del> del </button></li>`);
+            $("ul").append(`<li class="item"><input type="checkbox" class="check"> ${textTask} <button class="del" > del </button></li>`);
             $("input#task").val("");
-            //editCol();
+            editCol();
         }
 }
 
+//edit col
 function editCol() {
-    alert($("input.check").is(':checked').length);
+    $("p#all_task").text(`All task ${$("input.check").length} Done ${$("input.check:checked").length}`);
 }
 
 $(function () {
@@ -26,7 +28,6 @@ $(function () {
 
     $(document).on("click", ".del", function () {
         $(this).parent().remove();
-        
     });
     
     $("input#chek_all").change(function (e) { 
@@ -37,11 +38,28 @@ $(function () {
         {
             $("input.check").prop('checked', false);
         }
+        editCol();
     });
 
     $("button#del_all").click(function (e) {
         $("input.check:checked").parent().remove();
+        editCol();
     });
+
+
+    $("ul").on("change", "input.check" , function () {
+        editCol();
+    });
+    
+    // add dbclick
+    $( "ul" ).on( "dblclick", ".item", function(obj) {
+        alert(obj.html);
+    } );
+
+
+
+
+
 
 
 
