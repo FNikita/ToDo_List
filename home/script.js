b14b7a4cbaab05f34ad2f2f659a09acf2abf097a
Nik <nik.fuchkin@gmail.com>
2019-08-22 16:09:40 +0300

    Arlan sosed!

--- home/script.js
+++ home/script.js
@@ -1,30 +1,41 @@
-﻿function addElement(textTask) {
+﻿function addElement(textTask, redy) {
         if ( textTask.length != 0 ){
-            $("ul").append("<li id=\"item\">" + "<input type=\"checkbox\" >" + textTask  + "<button id=\"del\"  > del </button></li>");
+            $("ul").append(`<li id=\"item\"><input type=\"checkbox\" class=\"check\"> ${textTask} <button class=\"del\> del </button></li>`);
             $("input#task").val("");
         }
-}
-
-function delet(a) {
-    alert("del");
 }
 
 
 $(function () {
     var count = 0;
     var objEdit = {};
-    var redy = false;
 
     $("button#but").click(function (e) { 
-        addElement($("input#task").val());
+        addElement($("input#task").val(), redy);
     });
 
     $(document).keydown(function (e) { 
         if(e.code == "Enter") {
-         addElement($("input#task").val());
+         addElement($("input#task").val(), redy);
         }
     });
-    
 
+    $(document).on("click", ".del", function () {
+        $(this).parent().remove();
+    });
+    
+    $("input#chek_all").change(function (e) { 
+        if($("input#chek_all").is(':checked')) {
+            $("input.check").prop('checked', true);
+        }
+        else
+        {
+            $("input.check").prop('checked', false);
+        }
+    });
+
+    $("button#del_all").click(function (e) {
+        $("input.check:checked").parent().remove();
+    });
    
 });
